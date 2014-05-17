9398b416d27992c0d7a635768a43302d79cb68ec
coperd <lhcwhu@gmail.com>
2014-05-17 10:43:28 +0800

    daemonize the gmmagent

--- src/gmmagent.c
+++ src/gmmagent.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>       /* errno, perror */
-#include <time.h>        /* time(), ctime() */
-#include <inttypes.h>
+#include <errno.h>        /* errno, perror */
+#include <time.h>         /* time(), ctime() */
+#include <inttypes.h>     /* standard integer type */
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <sys/socket.h>    /* socket() */
+#include <netinet/in.h>    /* struct sockaddr */
+#include <sys/resource.h>  /* getrlimit() */
+#include <sys/stat.h>
+#include <syslog.h>        /* openlog() */
+#include <signal.h>        /* sigaction() */
+#include <unistd.h>        /* fork() */
+#include <fcntl.h>
+
 
 #define PROC_MEMINFO "/proc/meminfo"
 #define L_PORT       4588
@@ -58,6 +65,88 @@ float get_mem_usage(struct meminfo *miptr) /* 1~100(%) */
             + miptr->cached) / miptr->memtotal);
 }
 
+static void daemonize_agent(const char *cmd)
+{
+    pid_t pid;
+    struct rlimit rl;
+    struct sigaction sa;
+    int fd0, fd1, fd2;
+    int i;
+
+    /*
+     * clear file creation mask
+     */
+    umask(0);
+
+    pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        exit(errno);
+    }
+    /* 
+     * in parent process, terminate it 
+     */
+    if (pid > 0) {
+        exit(errno);
+    }
+
+    if (setsid() < 0) {
+        perror("setsid");
+        exit(errno);
+    }
+
+    sa.sa_handler = SIG_IGN;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    if (sigaction(SIGHUP, &sa, NULL) < 0) {
+        perror("sigaction");
+        exit(errno);
+    }
+
+    pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        exit(errno);
+    }
+    /* 
+     * in parent process, terminate it again 
+     */
+    if (pid > 0) {
+        exit(errno);
+    }
+
+    if (chdir("/") < 0) {
+        perror("chdir");
+        exit(errno);
+    }
+
+    /* close all file descriptors */
+    if (getrlimit(RLIMIT_NOFILE, &rl) < 0) {
+        perror("getrlimit");
+        exit(errno);
+    }
+    if (rl.rlim_max == RLIM_INFINITY)
+        rl.rlim_max = 1024;
+    for (i = 0; i < rl.rlim_max; i++)
+        close(i);
+
+    /*
+     * attach fd 0,1,2 to /dev/null
+     */
+    fd0 = open("/dev/null", O_RDWR);
+    fd1 = dup(0);
+    fd2 = dup(0);
+    
+    /*
+     * initialize the log file 
+     */
+    openlog(cmd, LOG_CONS, LOG_DAEMON);
+    if (fd0 != 0 || fd1 != 1 || fd2 != 2) {
+        syslog(LOG_ERR, "unexpected file descriptors %d %d %d", fd0, fd1, fd2);
+        exit(errno);
+    }
+}
+
 int main(int argc, char **argv)
 {
     int sockfd, connfd;
@@ -68,6 +157,8 @@ int main(int argc, char **argv)
     FILE *fp;
     uint32_t transbuf[NR_MEMINFO];
     int i;
+
+    daemonize_agent("gmmagent started");
 
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket");
